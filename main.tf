d8d0ee55c47a6156cbe2025d5bf5b4a1c83cb5f6
anila87 <anila.gokada.87@gmail.com>
2025-09-17 12:46:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+resource "google_project_service" "container" {
+  project = var.project
+  service = "container.googleapis.com"
+}
+
+resource "google_project_service" "compute" {
+  project = var.project
+  service = "compute.googleapis.com"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.zone                    # zonal cluster; change to region if you prefer regional
+  remove_default_node_pool = true
+  initial_node_count = 1                 # kept minimal because we manage node pools separately
+
+  logging_service    = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name     = "primary-node-pool"
+  cluster  = google_container_cluster.primary.name
+  location = google_container_cluster.primary.location
+
+  initial_node_count = var.node_count
+
+  node_config {
+    machine_type = var.machine_type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+    
+
+  autoscaling {
+    min_node_count = var.node_count
+    max_node_count = var.node_count
+  }
+}
