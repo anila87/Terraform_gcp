588a6c655c09b2b4f4bfbd6315b74b003177034d
anila87 <anila.gokada.87@gmail.com>
2025-09-17 12:47:26 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "project" {
+  description = "GCP project id"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region (for regional cluster or node pools)"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "Primary zone for a zonal cluster (if using zonal)"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "tf-gke-cluster"
+}
+
+variable "node_count" {
+  type    = number
+  default = 2
+}
+
+variable "machine_type" {
+  type    = string
+  default = "e2-medium"
+}
