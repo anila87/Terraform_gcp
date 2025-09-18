381f84189dae9d030a14abaea4a45977d39b54a1
Anila Gokada <anila.gokada.87@gmail.com>
2025-09-18 14:27:26 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "tfbu"       # your bucket name
-    prefix = "tfb"        # folder inside the bucket for storing the state
+    bucket = "tfbu"      
+    prefix = "tfb"        
   }
 }
