bf8fd49b81d59a2ebabec51bf2cff7e8ad4bc0bb
Anila Gokada <anila.gokada.87@gmail.com>
2025-09-18 11:27:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ resource "google_project_service" "compute" {
 }
 
 resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.zone                   
+  name                     = var.cluster_name
+  location                 = var.zone
   remove_default_node_pool = true
-  initial_node_count = 1                 
+  initial_node_count       = 1
 
   logging_service    = "logging.googleapis.com/kubernetes"
   monitoring_service = "monitoring.googleapis.com/kubernetes"
@@ -31,10 +31,28 @@ resource "google_container_node_pool" "primary_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
-    
+  }
 
   autoscaling {
     min_node_count = var.node_count
     max_node_count = var.node_count
   }
+}
+
+resource "google_storage_bucket" "tfstate_backup" {
+  name          = var.bucket_name
+  location      = var.location
+  force_destroy = true  
+  versioning {
+    enabled = true      
+  }
+
+  uniform_bucket_level_access = true
+}
+
+
+resource "google_storage_bucket_iam_member" "private_access" {
+  bucket = google_storage_bucket.tfstate_backup.name
+  role   = "roles/storage.objectAdmin"
+  member = "user:anila.gokada.8@gmail.com"
 }
