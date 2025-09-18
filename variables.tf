62ebceb5b0993d1c2249e7df9322774f02f39e77
Anila Gokada <anila.gokada.87@gmail.com>
2025-09-18 11:28:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -29,3 +29,14 @@ variable "machine_type" {
   type    = string
   default = "e2-medium"
 }
+
+variable "bucket_name" {
+  description = "Name of the GCS bucket for Terraform state backup"
+  type        = string
+}
+
+variable "location" {
+  description = "GCP region for the bucket"
+  type        = string
+  default     = "us-central1"
+}
