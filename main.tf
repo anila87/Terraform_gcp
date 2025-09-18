a7ba1e46947f2cfcd9075549804f1b047af8b501
Anila Gokada <anila.gokada.87@gmail.com>
2025-09-18 14:20:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
 resource "google_compute_instance_template" "default" {
   name         = "mig-template"
   machine_type = "e2-medium"
